083fcbbd0d7f5bb93c1e8fa3e5eab78f37b1aa1f
euichanhwang <pine6971@naver.com>
2023-08-01 22:17:37 +0900

    build: JUnit 만들기 복습을 위한 프로젝트 설정을 추가한다

---
+++ make-junit/src/main/java/com/example/makejunit/MakeJunitApplication.java
@@ -0,0 +1,13 @@
+package com.example.makejunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeJunitApplication.class, args);
+	}
+
+}
