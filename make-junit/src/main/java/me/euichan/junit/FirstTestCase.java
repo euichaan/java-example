2c2b230bddbaed4c2f218a6d36e84ea3acd9f0b5
euichanhwang <pine6971@naver.com>
2023-08-01 23:19:18 +0900

    feat: `픽스처 메서드`를 적용한다
    각각의 테스트 케이스들에게 공통적으로 수행되는 메서드 중 하나인 `setUp` 메서드를 적용한다

--- make-junit/src/main/java/me/euichan/junit/FirstTestCase.java
+++ make-junit/src/main/java/me/euichan/junit/FirstTestCase.java
@@ -5,27 +5,30 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class FirstTestCase extends TestCase {
 
+	private static int base;
+
 	public FirstTestCase(final String fName) {
 		super(fName);
 	}
 
-	public static void main(String[] args) {
-		new FirstTestCase("runTest").run(); // 각각의 테스트 케이스를 Command로 보고, 이를 실행하는 것은 run 메서드
-		new FirstTestCase("runTestMinus").run();
+	@Override
+	public void setUp() {
+		base = 10;
+		log.info("setUp invoked");
 	}
 
-	public void runTest() {
-		int sum = 10 + 10;
+	public static void main(String[] args) {
+		new FirstTestCase("runPlusTest").run(); // 각각의 테스트 케이스를 Command로 보고, 이를 실행하는 것은 run 메서드
+		new FirstTestCase("runMinusTest").run();
+	}
+
+	public void runPlusTest() {
+		int sum = base + 10;
 		Assert.assertTrue(sum == 20);
 	}
 
-	public void runTestMinus() {
-		int sum = 10 - 10;
-		Assert.assertTrue(sum == 0);
-	}
-
-	public void runTestFail() {
-		int sum = 10 + 10;
-		Assert.assertTrue(sum == 30);
+	public void runMinusTest() {
+		int minus = base - 10;
+		Assert.assertTrue(minus == 0);
 	}
 }
